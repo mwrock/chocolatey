97f151f0f1cc0b96f5e98e888e14a61cbc5c70b4
Matt Wrock <matt@mattwrock.com>
2012-09-19 09:52:33 -0700

    add paths to execs in proj file for users without env vars set

--- test.ps1
+++ test.ps1
@@ -1,2 +1,18 @@
 $scriptPath = (Split-Path -parent $MyInvocation.MyCommand.path)
-Start-Process $env:windir\System32\WindowsPowerShell\v1.0\powershell.exe -verb runas -ArgumentList "-NoProfile -ExecutionPolicy -Command 'CD `"$scriptPath`";Pester'"
+$identity  = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal( $identity )
+$isAdmin = $principal.IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator )
+$pesterDir = (dir $env:ChocolateyInstall/lib/Pester*)[-1]
+
+if(-not $isAdmin){
+    $psi = New-Object System.Diagnostics.ProcessStartInfo
+    $psi.FileName="$env:windir\System32\WindowsPowerShell\v1.0\powershell.exe"
+    $psi.Verb="runas"
+    $psi.Arguments="-NoProfile -ExecutionPolicy unrestricted -Command `". { CD '$scriptPath';. '$pesterDir\tools\bin\pester.bat'; if($lastExitCode -eq 0){exit} } `""
+    $s = [System.Diagnostics.Process]::Start($psi)
+    $s.WaitForExit()
+    $exitCode = $s.ExitCode
+    exit $exitCode
+} else {
+    . "$pesterDir\tools\bin\pester.bat"
+}
